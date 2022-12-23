c8843b283a66655d2612d4dd640e76a3d7a87627
Pawel Stankiewicz <40539746@live.napier.ac.uk>
2022-12-23 20:03:28 +0000

    +json, + flex, visibility slider kind of working

--- app.py
+++ app.py
@@ -1,53 +1,45 @@
 from flask import Flask, render_template, request
 from csv_handler import process_csv
 import csv
+import json
 import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_map():
-    # Read the CSV file
-    with open('data/12.csv') as f:  # f is a file object
-        reader = csv.DictReader(f)
+    # Read data from CSV file
+    df = pd.read_csv('data/12.csv')
+    # Convert to JSON
+    data = df.to_json(orient='records')
+    # Get list of numerical attributes. _get_num... returns new DF containing only the numerical columns
+    numerical_attributes = [col for col in df._get_numeric_data().columns]
 
-        # Store the data in a list
-        data = []
-        for row in reader:
-            data.append(row)
+# Pass data to Jinja2 template
+    return render_template('map.html', data=data, numerical_attributes=numerical_attributes)
 
-        # Get the numerical attributes from the CSV
-        numerical_attributes = []
-        for key in data[0].keys():
-            try:
-                float(data[0][key]) # Try to convert the value to a float
-                numerical_attributes.append(key)
-            except ValueError:
-                pass
 
-        # Render the template, passing in the data and numerical attributes
-        return render_template('index.html', data=data, numerical_attributes=numerical_attributes)
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
     if 'csv_file' not in request.files:
-        return render_template('index.html', error="No file uploaded")
+        return render_template('map.html', error="No file uploaded")
 
     csv_file = request.files['csv_file']
 
     if not csv_file or csv_file.filename == '':
-        return render_template('index.html', error="No file uploaded")
+        return render_template('map.html', error="No file uploaded")
 
     if not csv_file.filename.endswith('.csv'):
-        return render_template('index.html', error="Invalid file type")
+        return render_template('map.html', error="Invalid file type")
 
     # Check the file size and return an error if it is too large
     if csv_file.content_length > 20 * 1024 * 1024:
-        return render_template('index.html', error="File is too large (max 20 MB)")
+        return render_template('map.html', error="File is too large (max 20 MB)")
 
 
     data = process_csv(csv_file)
 
-    return render_template('index.html', data_dict=data)
+    return render_template('map.html', data_dict=data)
 
 if __name__ == '__main__':
     app.run()
